56254bc23e07f340717707028cd11e1ce02a612c
Shiho(mia) <mia.can2021@gamail.com>
2022-02-07 19:06:46 -0800

    firt commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, "dist"),
+      },
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/
+      }
+    ]
+  }
+}
